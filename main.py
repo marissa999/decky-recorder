003712a299c85922b2d94c8346810dbe8bb0dbde
marissa999 <github@littles.moe>
2023-01-12 19:54:43 +0000

    convert to tabs, fix bug in unloading

--- main.py
+++ main.py
@@ -9,38 +9,38 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/py_modules")
 import logging
 
 logging.basicConfig(filename="/tmp/template.log",
-                    format='Decky Recorder: %(asctime)s %(levelname)s %(message)s',
-                    filemode='w+',
-                    force=True)
-logger=logging.getLogger()
+					format='Decky Recorder: %(asctime)s %(levelname)s %(message)s',
+					filemode='w+',
+					force=True)
+logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 class Plugin:
 
-    recording_process = None
+	recording_process = None
 
-    async def start_recording(self):
-        logger.info("Starting recording")
-        if self.recording_process is not None:
-            logger.info("Error: recording already started")
-            pass
-        self.recording_process = "test"
-        pass
+	async def start_recording(self):
+		logger.info("Starting recording")
+		if self.recording_process is not None:
+			logger.info("Error: recording already started")
+			pass
+		self.recording_process = "test"
+		pass
 
-    async def end_recording(self):
-        logger.info("Stopping recording")
-        if self.recording_process is None:
-            logger.info("Error: No recording process to stop")
-            pass
-        self.recording_process = None
-        pass
+	async def end_recording(self):
+		logger.info("Stopping recording")
+		if self.recording_process is None:
+			logger.info("Error: No recording process to stop")
+			pass
+		self.recording_process = None
+		pass
 
-    async def _main(self):
-        logger.info("Loading...")
-        pass
+	async def _main(self):
+		logger.info("Loading...")
+		pass
 
-    async def _unload(self):
-        logger.info("Unloading...")
-        if self.recording_process is None:
-            Plugin.end_recording(self)
-        pass
+	async def _unload(self):
+		logger.info("Unloading...")
+		if self.recording_process is not None:
+			Plugin.end_recording(self)
+		pass
--- src/index.tsx
+++ src/index.tsx
@@ -1,54 +1,54 @@
 import {
-  ButtonItem,
-  definePlugin,
-  PanelSection,
-  PanelSectionRow,
-  ServerAPI,
-  staticClasses,
+	ButtonItem,
+	definePlugin,
+	PanelSection,
+	PanelSectionRow,
+	ServerAPI,
+	staticClasses,
 } from "decky-frontend-lib";
 
 import {
-  VFC, 
-  useState
+	VFC, 
+	useState
 } from "react";
 
 import { FaVideo } from "react-icons/fa";
 
 const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
 
-  const [recordingStarted, setRecordingStarted] = useState(false);
+	const [recordingStarted, setRecordingStarted] = useState(false);
 
-  const recordingButtonPress = async() => {
-    if (recordingStarted === false){
-      setRecordingStarted(true);
-      await serverAPI.callPluginMethod('start_recording', {});
-    } else {
-      setRecordingStarted(false);
-      await serverAPI.callPluginMethod('end_recording', {});
-    }
-  }
+	const recordingButtonPress = async() => {
+	if (recordingStarted === false){
+		setRecordingStarted(true);
+		await serverAPI.callPluginMethod('start_recording', {});
+	} else {
+		setRecordingStarted(false);
+		await serverAPI.callPluginMethod('end_recording', {});
+	}
+	}
 
-  return (
-    <PanelSection>
-      <PanelSectionRow>
-        <ButtonItem 
-          label="Recordings will be saved to ~/Videos"
-          bottomSeparator="none"
-          layout="below"
-          onClick={() => {
-            recordingButtonPress();
-          }}>
-          {recordingStarted === false ? "Start Recording" : "Stop Recording"}
-        </ButtonItem>
-      </PanelSectionRow>
-    </PanelSection>
-  );
+	return (
+	<PanelSection>
+		<PanelSectionRow>
+			<ButtonItem 
+				label="Recordings will be saved to ~/Videos"
+				bottomSeparator="none"
+				layout="below"
+				onClick={() => {
+				recordingButtonPress();
+				}}>
+				{recordingStarted === false ? "Start Recording" : "Stop Recording"}
+			</ButtonItem>
+		</PanelSectionRow>
+	</PanelSection>
+	);
 };
 
 export default definePlugin((serverApi: ServerAPI) => {
-  return {
-    title: <div className={staticClasses.Title}>Decky Recorder</div>,
-    content: <Content serverAPI={serverApi} />,
-    icon: <FaVideo />,
-  };
+	return {
+		title: <div className={staticClasses.Title}>Decky Recorder</div>,
+		content: <Content serverAPI={serverApi} />,
+		icon: <FaVideo />,
+	};
 });
