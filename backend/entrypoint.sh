312a3f19771fd268f8e73c9f8568f78f68dda9d1
marissa999 <github@littles.moe>
2023-01-14 02:02:00 +0000

    switching to building with a docker file

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 set -e
 
+rm -rf /backend/out
+
 cd /backend
+
 mkdir -p /backend/out/plugins/vaapi
 mkdir -p /backend/out/plugins/pipewire
 mkdir -p /backend/out/plugins/bad
 mkdir -p /backend/out/plugins/good
 mkdir -p /backend/out/libs
 
-cp /usr/lib/gstreamer-1.0/libgstvaapi.so /backend/out/plugins/
+cp /usr/lib/gstreamer-1.0/libgstvaapi.so /backend/out/plugins/vaapi
 
-cp /usr/lib/gstreamer-1.0/libgstpipewire.so /backend/out/plugins/
+cp /usr/lib/gstreamer-1.0/libgstpipewire.so /backend/out/plugins/pipewire
 
 cp /usr/lib/libgstcodecparsers* /backend/out/libs/
-cp /usr/lib/gstreamer-1.0/libgstvideoparsersbad.so /backend/out/plugins/
+cp /usr/lib/gstreamer-1.0/libgstvideoparsersbad.so /backend/out/plugins/bad
 
-cp /usr/lib/gstreamer-1.0/libgstisomp4.so /backend/out/plugins/
-cp /usr/lib/gstreamer-1.0/libgstpulseaudio.so /backend/out/plugins/
-cp /usr/lib/gstreamer-1.0/libgstlame.so /backend/out/plugins/
+cp /usr/lib/gstreamer-1.0/libgstisomp4.so /backend/out/plugins/good
+cp /usr/lib/gstreamer-1.0/libgstpulseaudio.so /backend/out/plugins/good
+cp /usr/lib/gstreamer-1.0/libgstlame.so /backend/out/plugins/good
 
 # Heavily inspired by https://github.com/Epictek/DeckyStream/blob/ed63bc4fbb83d0cdcb277630f57de6042b36ac00/backend/entrypoint.sh
