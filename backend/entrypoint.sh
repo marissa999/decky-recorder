f11ff49a6bd1bf459a5c6798ba1f1efba287a12a
marissa999 <github@littles.moe>
2023-01-14 01:44:31 +0000

    added source reference

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -18,3 +18,5 @@ cp /usr/lib/gstreamer-1.0/libgstvideoparsersbad.so /backend/out/plugins/
 cp /usr/lib/gstreamer-1.0/libgstisomp4.so /backend/out/plugins/
 cp /usr/lib/gstreamer-1.0/libgstpulseaudio.so /backend/out/plugins/
 cp /usr/lib/gstreamer-1.0/libgstlame.so /backend/out/plugins/
+
+# Heavily inspired by https://github.com/Epictek/DeckyStream/blob/ed63bc4fbb83d0cdcb277630f57de6042b36ac00/backend/entrypoint.sh
