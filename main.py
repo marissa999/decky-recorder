3030e636a066c470675316511d5b7eff23f5b9fc
marissa999 <github@littles.moe>
2023-01-15 21:19:40 +0000

    started adding rtsp (ui done)

--- main.py
+++ main.py
@@ -8,12 +8,27 @@ DEPSPATH = "/home/deck/homebrew/plugins/decky-recorder/backend/out"
 DEPSPLUGINSPATH = DEPSPATH + "/plugins"
 DEPSLIBSSPATH = DEPSPATH + "/libs"
 
+import logging
+
+logging.basicConfig(filename="/tmp/decky-recorder.log",
+					format='Decky Recorder: %(asctime)s %(levelname)s %(message)s',
+					filemode='w+',
+					force=True)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+std_out_file = open('/tmp/decky-recorder-std-out.log', 'w')
+std_err_file = open('/tmp/decky-recorder-std-err.log', 'w')
+
 class Plugin:
 
 	_recording_process = None
+	_mode = "localFile"
 
 	async def start_recording(self):
+		logger.info("Starting recording")
 		if Plugin.is_recording(self) == True:
+			logger.info("Error: Already recording")
 			return
 		os.environ["XDG_RUNTIME_DIR"] = "/run/user/1000"
 		os.environ["XDG_SESSION_TYPE"] = "wayland"
@@ -25,19 +40,36 @@ class Plugin:
 		filename = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
 		monitor = subprocess.getoutput("pactl get-default-sink") + ".monitor"
 		cmd = "GST_VAAPI_ALL_DRIVERS=1 GST_PLUGIN_PATH={} LD_LIBRARY_PATH={} gst-launch-1.0 -e -vvv pipewiresrc do-timestamp=true ! vaapipostproc ! queue ! vaapih264enc ! h264parse ! mp4mux name=sink ! filesink location=/home/deck/Videos/{}.mp4 pulsesrc device=\"{}\" ! audioconvert ! lamemp3enc target=bitrate bitrate=128 cbr=true ! sink.audio_0".format(DEPSPLUGINSPATH, DEPSLIBSSPATH, filename, monitor)
-
-		self._recording_process = subprocess.Popen(cmd, shell = True)
+		self._recording_process = subprocess.Popen(cmd, shell = True, stdout = std_out_file, stderr = std_err_file)
+		logger.info("Recording started!")
 		return
 
 	async def end_recording(self):
+		logger.info("Stopping recording")
 		if Plugin.is_recording(self) == False:
+			logger.info("Error: No recording process to stop")
 			return
 		self._recording_process.send_signal(signal.SIGINT)
 		self._recording_process = None
+		logger.info("Recording stopped!")
 		return
 
 	async def is_recording(self):
+		logger.info("Is recording? " + str(self._recording_process is not None))
 		return self._recording_process is not None
+
+	async def set_current_mode(self, mode):
+		logger.info("New mode: " + mode)
+		self._mode = mode
+
+	async def current_mode(self):
+		logger.info("Current mode: " + self._mode)
+		return self._mode
+
+	async def wlan_ip(self):
+		ip = subprocess.getoutput("ip -f inet addr show wlan0 | sed -En -e 's/.*inet ([0-9.]+).*/\\1/p'")
+		logger.info("IP: " + ip)
+		return ip
 
 	async def _main(self):
 		return
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import {
 	PanelSectionRow,
 	ServerAPI,
 	staticClasses,
+	Dropdown,
+	DropdownOption,
+	SingleDropdownOption,
 	Router
 } from "decky-frontend-lib";
 
@@ -20,9 +23,27 @@ const DeckyRecorder: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
 
 	const [recordingStarted, setRecordingStarted] = useState(false);
 
+	const [ip, setIp] = useState("127.0.0.1");
+
+	const modeOptionLocalFile = {data: "localFile", label: "Local File"} as SingleDropdownOption
+	const modeOptionRtsp = {data: "rtsp", label: "RTSP-Server (OBS)"} as SingleDropdownOption;
+	const modeOptions: DropdownOption[] = [modeOptionLocalFile, modeOptionRtsp];
+	const [currentMode, setCurrentMode] = useState(modeOptionLocalFile);
+
 	const initState = async () => {
 		const is_recording_response = await serverAPI.callPluginMethod('is_recording', {})
 		setRecordingStarted(is_recording_response.result as boolean);
+
+		const ip_response = await serverAPI.callPluginMethod('wlan_ip', {})
+		setIp(ip_response.result as string);
+
+		const currentModeResponse = await serverAPI.callPluginMethod('current_mode', {})
+		const currentModeResponseString = currentModeResponse.result as string;
+		if (currentModeResponseString == modeOptionLocalFile.data){
+			setCurrentMode(modeOptionLocalFile)
+		} else if (currentModeResponseString == modeOptionRtsp.data){
+			setCurrentMode(modeOptionRtsp)
+		}
 	}
 
 	const recordingButtonPress = async() => {
@@ -36,21 +57,67 @@ const DeckyRecorder: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
 		}
 	}
 
+	const strDefaultLabelText = (): string => {
+		if (currentMode !== null){
+			return currentMode.label as string
+		}
+		return "Select a mode";
+	}
+
+	const getLabelText = (): string => {
+		if (currentMode.data === modeOptionLocalFile.data){
+			return "Recordings will be saved to ~/Videos";
+		} else if (currentMode.data === modeOptionRtsp.data){
+			return "RTSP Stream will listen on " + ip + ":5000";
+		}
+		return "error?" + currentMode.data
+	}
+
+	const getButtonText = (): string => {
+		if (recordingStarted === false){
+			if (currentMode.data === modeOptionLocalFile.data){
+				return "Start Recording";
+			} else if (currentMode.data === modeOptionRtsp.data){
+				return "Start Streaming";
+			}
+		} else {
+			if (currentMode.data === modeOptionLocalFile.data){
+				return "Stop Recording";
+			} else if (currentMode.data === modeOptionRtsp.data){
+				return "Stop Streaming";
+			}
+		}
+		return "Unknown state, please restart"
+	}
+
 	useEffect(() => {
 		initState();
-	  }, []);
+	}, []);
 
 	return (
 	<PanelSection>
 		<PanelSectionRow>
 			<ButtonItem 
-				label="Recordings will be saved to ~/Videos"
+				label={getLabelText()}
 				layout="below"
 				onClick={() => {
 					recordingButtonPress();
 				}}>
-				{recordingStarted === false ? "Start Recording" : "Stop Recording"}
+				{getButtonText()}
 			</ButtonItem>
+		</PanelSectionRow>
+		<PanelSectionRow>
+			<Dropdown
+				disabled={recordingStarted === true}
+				menuLabel="Select the mode you want use"
+				strDefaultLabel={strDefaultLabelText()}
+				rgOptions={modeOptions}
+				selectedOption={currentMode}
+				onChange={(newMode) => {
+					serverAPI.callPluginMethod('set_current_mode', {mode: newMode.data});
+					setCurrentMode(newMode);
+				}}
+			/>
 		</PanelSectionRow>
 	</PanelSection>
 	);
