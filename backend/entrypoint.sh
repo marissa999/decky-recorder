aaf1ee7a4e9d188caae82d0de9151ff1e23c9c34
marissa999 <github@littles.moe>
2023-01-29 13:23:23 +0000

    modifying entrypoint.sh cant remove folder for some reason, cicd claims device busy dunno

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,13 +2,9 @@
 set -e
 
 OUTDIR="/backend/out"
-if [ -d "$OUTDIR" ]; then
-    sudo rm -rf $OUTDIR
-fi
-
+mkdir -p $OUTDIR
 cd /backend
 
-mkdir -p /backend/out
 cp -r /pacman/usr/lib/* /backend/out
 
 # Heavily inspired by https://github.com/Epictek/DeckyStream/blob/ed63bc4fbb83d0cdcb277630f57de6042b36ac00/backend/entrypoint.sh
