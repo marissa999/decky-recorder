d142a89d156c31e0fe69c14869d00832a51d5794
Liam <22931076+Biyoni@users.noreply.github.com>
2023-02-03 14:23:39 +0000

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e
 
-OUTDIR="/backend/out"
+OUTDIR="/backend/decky-out"
 if [ -d "$OUTDIR" ]; then
     sudo rm -rf $OUTDIR
 fi
 
 cd /backend
 
-mkdir -p /backend/out
-cp -r /pacman/usr/lib/* /backend/out
+mkdir -p /backend/decky-out
+cp -r /pacman/usr/lib/* /backend/decky-out
 
 # Heavily inspired by https://github.com/Epictek/DeckyStream/blob/ed63bc4fbb83d0cdcb277630f57de6042b36ac00/backend/entrypoint.sh
